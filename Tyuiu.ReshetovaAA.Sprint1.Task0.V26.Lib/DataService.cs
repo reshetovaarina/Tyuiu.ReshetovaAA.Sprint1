2114db83c3674dc83a10b4bfa3e86f11054e2fd0
reshetovaarina <tyuiu.reshetovaaa@outlook.com>
2025-09-27 02:49:39 +0500

    Add project files.

---
+++ Tyuiu.ReshetovaAA.Sprint1.Task0.V26.Lib/DataService.cs
@@ -0,0 +1,11 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+namespace Tyuiu.ReshetovaAA.Sprint1.Task0.V26.Lib
+{
+    public class DataService : ISprint1Task0V26
+    {
+        public double Calculate()
+        {
+            return (4 * 5) / 2 - (18 / 2) / 3;
+        }
+    }
+}
---
+++ Tyuiu.ReshetovaAA.Sprint1.Task0.V26.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.ReshetovaAA.Sprint1.Task0.V26.Lib;
+namespace Tyuiu.ReshetovaAA.Sprint1.Task0.V26.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            var res = ds.Calculate();
+            Assert.AreEqual(7, res);
+        }
+    }
+}
