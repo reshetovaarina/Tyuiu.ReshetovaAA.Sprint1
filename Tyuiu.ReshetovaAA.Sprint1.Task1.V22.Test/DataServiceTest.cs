641a0887685b8da4f17daa52044ab13357259a83
reshetovaarina <tyuiu.reshetovaaa@outlook.com>
2025-09-27 03:00:36 +0500

    Task1

---
+++ Tyuiu.ReshetovaAA.Sprint1.Task1.V22.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.ReshetovaAA.Sprint1.Task1.V22.Lib;
+namespace Tyuiu.ReshetovaAA.Sprint1.Task1.V22.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 1.0;
+            double y = 2.0;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(3, res);
+        }
+    }
+}
---
+++ Tyuiu.ReshetovaAA.Sprint1.Task1.V22/Program.cs
@@ -0,0 +1,41 @@
+﻿using Tyuiu.ReshetovaAA.Sprint1.Task1.V22.Lib;
+DataService ds = new DataService();
+
+
+Console.Title = "Спринт #1 | Выполнила: Решетова А. А.  | СМАРТб-25-1";
+Console.WriteLine("***************************************************************************************************");
+Console.WriteLine("*Спринт #1                                                                                        *");
+Console.WriteLine("*Тема: Базовые навыки работы в C#                                                                 *");
+Console.WriteLine("*Задание #0                                                                                       *");
+Console.WriteLine("*Вариант #22                                                                                      *");
+Console.WriteLine("*Выполнила: Решетова А. А.  | СМАРТб-25-1                                                         *");
+Console.WriteLine("***************************************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                                        *");
+Console.WriteLine("* Написать контрольную программу на C#, которая вычисляет выражение (5+x)/(y*x)                   *");
+Console.WriteLine("* И печатает результат на экране.                                                                 *");
+Console.WriteLine("*                                                                                                 *");
+Console.WriteLine("***************************************************************************************************");
+Console.WriteLine("*ИСХОДНЫЕ ДАННЫЕ:                                                                                 *");
+Console.WriteLine("***************************************************************************************************");
+Console.WriteLine("* (5+x)/(y*x)                                                                                     *");
+
+
+double x, y;
+
+
+Console.WriteLine("Введите значение X:");
+x = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите значение Y:");
+y = Convert.ToDouble(Console.ReadLine());
+
+
+Console.WriteLine("***************************************************************************************************");
+Console.WriteLine("* Результат                                                                                       *");
+Console.WriteLine("***************************************************************************************************");
+
+
+Console.WriteLine(ds.Calculate(x, y));
+
+
+Console.ReadLine();
