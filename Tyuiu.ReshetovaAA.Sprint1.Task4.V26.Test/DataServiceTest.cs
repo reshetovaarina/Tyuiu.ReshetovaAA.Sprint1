d756e47cd36c6a2926a47f3f75d07ecac609bd60
reshetovaarina <tyuiu.reshetovaaa@outlook.com>
2025-09-30 23:36:52 +0500

    Task4

--- Tyuiu.ReshetovaAA.Sprint1.Task4.V26.Test/DataServiceTest.cs
+++ Tyuiu.ReshetovaAA.Sprint1.Task4.V26.Test/DataServiceTest.cs
@@ -8,10 +8,9 @@ namespace Tyuiu.ReshetovaAA.Sprint1.Task4.V26.Test
         public void ValidExpression()
         {
             DataService ds = new DataService();
-            double x = 0.1;
-            double y = 0.29;
-            double expected = -0.062;
-            double wait = Math.Round((Math.Atan(x) + y) / Math.Exp(y+z), 3);
+            double x = 1;
+            double y = 1;
+            double wait = Math.Round((Math.Atan(x) + y) / Math.Exp(y), 3);
 
             var res = ds.Calculate(x, y);
 
