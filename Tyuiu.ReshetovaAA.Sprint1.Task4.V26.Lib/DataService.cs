46860bb2b42ebd5da41320bdfc57b3d1ce8088e4
reshetovaarina <tyuiu.reshetovaaa@outlook.com>
2025-09-30 23:10:31 +0500

    Test 4

---
+++ Tyuiu.ReshetovaAA.Sprint1.Task4.V26.Lib/DataService.cs
@@ -0,0 +1,13 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+    namespace Tyuiu.ReshetovaAA.Sprint1.Task4.V26.Lib
+{
+    public class DataService : ISprint1Task4V26
+    {
+        public double Calculate(double x, double y)
+        {
+            double z = 1;
+            double res = (Math.Atan(x) + y) / Math.Exp(y + z);
+            return Math.Round(res, 3);
+        }
+    }
+}
---
+++ Tyuiu.ReshetovaAA.Sprint1.Task4.V26.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using  Tyuiu.ReshetovaAA.Sprint1.Task4.V26.Lib;
+namespace Tyuiu.ReshetovaAA.Sprint1.Task4.V26.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 1;
+            double y = 1;
+            double z = 1;
+            double wait = Math.Round((Math.Atan(x) + y) / Math.Exp(y + z), 3);
+
+            var res = ds.Calculate(x, y);
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
