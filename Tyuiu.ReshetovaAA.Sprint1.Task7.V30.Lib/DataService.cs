3043576668c49800f2132dfba65fe4158af2707b
reshetovaarina <tyuiu.reshetovaaa@outlook.com>
2025-10-15 21:23:46 +0500

    Task7

--- Tyuiu.ReshetovaAA.Sprint1.Task7.V30.Lib/DataService.cs
+++ Tyuiu.ReshetovaAA.Sprint1.Task7.V30.Lib/DataService.cs
@@ -6,7 +6,7 @@ namespace Tyuiu.ReshetovaAA.Sprint1.Task7.V30.Lib
         public double Calculate(double x, double y)
         {
             double res = x + Math.Exp(x) + (Math.Sin(Math.Pow(x, 5)) + Math.Pow(x, 3)) / Math.Pow(3, x) + Math.Pow(y, 5) / Math.Pow(5, y);
-            return res;
+            return Math.Round(res, 3);
         }
     }
 }
--- Tyuiu.ReshetovaAA.Sprint1.Task7.V30.Test/DataServiceTest.cs
+++ Tyuiu.ReshetovaAA.Sprint1.Task7.V30.Test/DataServiceTest.cs
@@ -10,7 +10,7 @@ namespace Tyuiu.ReshetovaAA.Sprint1.Task7.V30.Test
             DataService ds = new DataService();
             double x = 1;
             double y = 2;
-            double wait = 5.612105490061677 ;
+            double wait = Math.Round(x + Math.Exp(x) + (Math.Sin(Math.Pow(x, 5)) + Math.Pow(x, 3)) / Math.Pow(3, x) + Math.Pow(y, 5) / Math.Pow(5, y), 3);
             var res = ds.Calculate(x, y);
             Assert.AreEqual(wait, res);
 
